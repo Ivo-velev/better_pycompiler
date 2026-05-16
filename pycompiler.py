03c6a895d4f2d28964dd3e00740090da7d367a9d
Ivo Velev <ivovelev@Ivos-Mac-mini.local>
2026-05-16 17:06:21 +0100

    added more test cases and logic for lexer

--- pycompiler.py
+++ pycompiler.py
@@ -1,12 +1,26 @@
 class Token:
-    def __init__(self, type, value):
+    def __init__(self, type, value, line):
         self.type = type
         self.value = value
+        self.line = line
 
-def lexer(line):
-    chars = line.split()
+def lexer(input):
+    lines = input.splitlines()
     tokens = []
-    for char in chars:
-        if char.isnumeric():
-            tokens.append(Token(type="NUMBER", value=int(char)))
+    for count, line in enumerate(lines):
+        chars = line.split()
+        line_number = count+1
+        for char in chars:
+            if char.isnumeric():
+                tokens.append(Token(type="NUMBER", value=int(char), line=line_number))
+            elif char in ["while", "end"]:
+                tokens.append(Token(type="KEYWORD", value=char, line=line_number))
+            elif char.isalpha():
+                tokens.append(Token(type="IDENT", value=char, line=line_number))
+            elif char == "=":
+                tokens.append(Token(type="ASSIGN", value=char, line=line_number))
+            elif char in ["+", "-", "*", ">="]:
+                tokens.append(Token(type="OPERATOR", value=char, line=line_number))
+            else:
+                raise SyntaxError("Unexpected character '%s'" % char)
     return tokens
--- test_lexer.py
+++ test_lexer.py
@@ -7,3 +7,77 @@ def test_lexer_parses_single_number():
     tokens = lexer(line)
 
     assert tokens[0].type == "NUMBER"
+    assert tokens[0].value == 5
+    assert tokens[0].line == 1
+
+def test_lexer_parses_single_indentifier():
+    line = "n"
+
+    tokens = lexer(line)
+
+    assert tokens[0].type == "IDENT"
+    assert tokens[0].value == "n"
+    assert tokens[0].line == 1
+
+def test_lexer_parses_keyword():
+    line = "while"
+
+    tokens = lexer(line)
+
+    assert tokens[0].type == "KEYWORD"
+    assert tokens[0].value == "while"
+    assert tokens[0].line == 1
+
+def test_lexer_parser_numbers_on_seperate_lines():
+    input = "5\n5"
+
+    tokens = lexer(input)
+
+    assert len(tokens) == 2
+
+    assert tokens[0].type == "NUMBER"
+    assert tokens[0].value == 5
+    assert tokens[0].line == 1
+    assert tokens[1].type == "NUMBER"
+    assert tokens[1].value == 5
+    assert tokens[1].line == 2
+
+def test_lexer_parses_assign():
+    input = "="
+
+    tokens = lexer(input)
+
+    assert tokens[0].type == "ASSIGN"
+    assert tokens[0].value == "="
+    assert tokens[0].line == 1
+
+@pytest.mark.parametrize("operator", ["+", "-", "*", ">="])
+def test_lexer_parses_operator(operator):
+    tokens = lexer(operator)
+
+    assert tokens[0].type == "OPERATOR"
+    assert tokens[0].value == operator
+    assert tokens[0].line == 1
+
+def test_unknown_char_raises_syntax_error():
+    input = "@"
+
+    with pytest.raises(SyntaxError):
+        lexer(input)
+
+def test_lexer_parses_assignment():
+    input = "n = 5"
+
+    tokens = lexer(input)
+
+    assert tokens[0].type == "IDENT"
+    assert tokens[0].value == "n"
+    assert tokens[0].line == 1
+
+    assert tokens[1].type == "ASSIGN"
+    assert tokens[1].value == "="
+    assert tokens[1].line == 1
+
+    assert tokens[2].type == "NUMBER"
+    assert tokens[2].value == 5
+    assert tokens[2].line == 1
